03d80b2e0d3d848cbc287eb25c387001ca40799d
goriori <vladislav.borisov2010@gmail.com>
2024-02-06 13:41:37 +0300

    Init project

---
+++ front-end/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import VLoader from '@/components/VLoader.vue'
+import { useStateStore } from '@/store/stateStore'
+import VButtonExit from "./components/VButtonExit.vue";
+
+const route = useRoute()
+const stateStore = useStateStore()
+</script>
+
+<template>
+  <main class="main">
+    <VButtonExit/>
+    <RouterView v-slot="{ Component }">
+      <template v-if="Component">
+        <transition name="fade-page" mode="out-in">
+          <suspense>
+            <component :is="Component" :key="route.fullPath"></component>
+          </suspense>
+        </transition>
+      </template>
+    </RouterView>
+    <VLoader v-if="stateStore.isLoading" />
+  </main>
+</template>
+
+<style scoped></style>
